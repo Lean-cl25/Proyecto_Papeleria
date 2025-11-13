a2f7e4a3eb5c0df6822e87fe4b98bebc354f96b2
Leo0593 <leandroclavijo854@gmail.com>
2025-11-13 09:15:20 +0100

    Incorporacion boton para retornar al menu Principal desde la ventana de Ventas

--- src/main/java/org/example/controllers/Ventas/VentasController.java
+++ src/main/java/org/example/controllers/Ventas/VentasController.java
@@ -3,6 +3,7 @@ package org.example.controllers.Ventas;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -31,6 +32,11 @@ public class VentasController {
     @FXML
     public void initialize() {
         configurarTabla();
+    }
+
+    @FXML
+    void MenuPrincipal(ActionEvent event) {
+        cambiarPantalla(event, "/view/main-view.fxml");
     }
 
     private void configurarTabla() {
@@ -128,5 +134,26 @@ public class VentasController {
         alert.setHeaderText(null);
         alert.setContentText(mensaje);
         alert.showAndWait();
+    }
+
+    //Cambio de Pantallas parte Principal
+    private void cambiarPantalla(ActionEvent event, String fxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // 🔹 Guarda el tamaño actual antes de cambiar la escena
+            double ancho = stage.getWidth();
+            double alto = stage.getHeight();
+
+            Scene nuevaScene = new Scene(root, ancho, alto);
+            stage.setScene(nuevaScene);
+            stage.setMaximized(true); // asegura que siga maximizado
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
